0760c4ba43c6a838545c9f108669329f40ca1f3d
Patrick Rivera <volrathxiii@gmail.com>
2016-02-08 20:41:03 +0800

    relay test

---
+++ bin/relay_on_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+
+#set gpio pin
+pin = 10
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(pin, GPIO.OUT)
+GPIO.output(pin, GPIO.LOW)
+#GPIO.cleanup()
